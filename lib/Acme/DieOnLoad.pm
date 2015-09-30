7cbd9a8815b522614fcc4f2a024168b517f4db71
Karen Etheridge <ether@cpan.org>
2015-09-29 21:32:11 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.105, github profile.

---
+++ lib/Acme/DieOnLoad.pm
@@ -0,0 +1,41 @@
+use strict;
+use warnings;
+package Acme::DieOnLoad;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sts=4 sw=4 tw=115 et :
+
+our $VERSION = '0.001';
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Acme::DieOnLoad;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
